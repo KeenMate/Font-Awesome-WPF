aae8ff4b755cbf3c530d4e007a6b6d21d0203132
Ondrej Valenta <ondrej.valenta@keenmate.com>
2020-08-22 20:37:28 +0200

    Windows Core version

---
+++ src/Core/FontAwesome.WPFCore/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows.Markup;
+
+//[assembly: AssemblyTitle("Font Awesome WPF Core")]
+//[assembly: AssemblyDescription("Wpf components for the iconic font and CSS toolkit Font-Awesome")]
+//[assembly: AssemblyConfiguration("")]
+//[assembly: AssemblyCompany("charri")]
+//[assembly: AssemblyProduct("FontAwesome.WPF")]
+//[assembly: AssemblyCopyright("Copyright © 2015-2020")]
+//[assembly: AssemblyTrademark("")]
+//[assembly: AssemblyCulture("")]
+
+//[assembly: ComVisible(false)]
+//[assembly: Guid("c1ef07d8-c739-421a-a811-278c9341677c")]
+
+//[assembly: AssemblyVersion("4.8.0.0")]
+//[assembly: AssemblyFileVersion("4.8.0.0")]
+
+[assembly: XmlnsPrefix("http://schemas.fontawesome.io/icons/", "fa")]
+[assembly: XmlnsDefinition("http://schemas.fontawesome.io/icons/", "FontAwesome.WPF")]
+[assembly: XmlnsDefinition("http://schemas.fontawesome.io/icons/", "FontAwesome.WPF.Converters")]
